30735cfdc82cf2b1e2040933cbd72910e728e632
David Poole <hymnally.yours@gmail.com>
2026-01-09 17:05:17 -0500

    Created basic initial application structure, added basic constants needed for starting application development.

---
+++ src/utils/constants/audio_settings.py
@@ -0,0 +1,8 @@
+from typing import Final
+
+AUDIO_SETTINGS: Final = "Audio Settings"
+AUDIO_DEVICE: Final = "Audio Device"
+NUMBER_CHANNELS: Final = "Number of Channels"
+BLOCK_SIZE: Final = "Block Size"
+SAMPLERATE: Final = "Samplerate"
+LATENCY: Final = "Latency"
