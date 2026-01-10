443064b2116fd09f985e561dc0d74e9d4eb4f0c6
David Poole <hymnally.yours@gmail.com>
2026-01-10 11:34:08 -0500

    Unified utils module into a single constants.py file. Expanded the entries to include lists (tuples) and dictionaries. Will include important functions in the future.

--- src/utils/constants/audio_settings.py
+++
@@ -1,8 +0,0 @@
-from typing import Final
-
-AUDIO_SETTINGS: Final = "Audio Settings"
-AUDIO_DEVICE: Final = "Audio Device"
-NUMBER_CHANNELS: Final = "Number of Channels"
-BLOCK_SIZE: Final = "Block Size"
-SAMPLERATE: Final = "Samplerate"
-LATENCY: Final = "Latency"
